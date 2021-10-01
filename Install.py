e7cb770037c39405e4d90a72340917f2033508f6
cvip <80930896+ph-hub@users.noreply.github.com>
2021-09-30 20:23:05 -0700

    Create Install.py

---
+++ Install.py
@@ -0,0 +1,8 @@
+from os import system
+system("cls && title LINKVERTISE BYPASSER INSTALLER")
+system("pip install gratient")
+system("pip install requests")
+system("cls")
+print("All the shit has been installed ez")
+system("pause >nul")
+exit()
